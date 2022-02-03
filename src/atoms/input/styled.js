595f17cbd3ed4aac3b19b03ef100c0addc6b7906
Niko Juric <nikojuric1998@gmail.com>
2022-02-03 16:06:54 +0100

    fix: linting error #3

--- src/atoms/input/styled.js
+++ src/atoms/input/styled.js
@@ -6,7 +6,7 @@ const StyledInput = styled.input`
 	border-style: none;
 	border-radius: ${pxToRem(9)};
 	background: #fff;
-	filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+	filter: drop-shadow(0 4px 4px rgba(0, 0, 0, 0.25));
 	font-family: Roboto, serif;
 	font-size: 1em;
 	font-weight: 400;
--- src/atoms/logo/styled.js
+++ src/atoms/logo/styled.js
@@ -2,6 +2,5 @@ import styled from "@emotion/styled";
 import Image from "next/image";
 
 export const StyledLogo = styled(Image)`
-	filter: drop-shadow(0px 4px 4px rgba(0,0,0,0.25));
+	filter: drop-shadow(0 4px 4px rgba(0, 0, 0, 0.25));
 `;
-
