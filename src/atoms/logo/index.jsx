b5836d8817e04e8eeee6e05d4b852508c8b1054b
Niko Juric <nikojuric1998@gmail.com>
2022-02-04 15:42:26 +0100

    refactor: logo component out of extended logo

---
+++ src/atoms/logo/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../../../public/logo.png";
+import { StyledLogo } from "./styled";
+
+export const Logo = ({ ...props }) => {
+	return (
+		<div>
+			<StyledLogo
+				src={logo}
+				alt="This image shows an owl which is the logo for Hoo!"
+				{...props}
+			/>
+		</div>
+	);
+};
--- src/molecules/extended-logo/index.jsx
+++ src/molecules/extended-logo/index.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import {StyledLogo} from "../../atoms/logo/styled";
-import logo from "../../../public/logo.png";
-import {StyledHeadline} from "../../atoms/logo-headline/styled";
-import {StyledMarks} from "../../atoms/logo-marks/styled";
-import {StyledLogoDiv} from "./styled";
+import { StyledHeadline } from "../../atoms/logo-headline/styled";
+import { StyledMarks } from "../../atoms/logo-marks/styled";
+import { StyledLogoDiv } from "./styled";
+import { Logo } from "../../atoms/logo";
 
-const ExtendedLogo = ({src, alt, ...props}) => {
+const ExtendedLogo = ({ src, alt, ...props }) => {
 	return (
 		<StyledLogoDiv>
-			<StyledLogo src={logo} alt="This image shows an owl which is the logo for Hoo!" {...props} />
-			<StyledHeadline>Hoo<StyledMarks>!?</StyledMarks></StyledHeadline>
+			<Logo {...props} />
+			<StyledHeadline>
+				Hoo<StyledMarks>!?</StyledMarks>
+			</StyledHeadline>
 		</StyledLogoDiv>
-	)
-}
+	);
+};
 
 export default ExtendedLogo;
