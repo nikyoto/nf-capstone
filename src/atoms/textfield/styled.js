3eacbd1caeef912e60a3e90e7294e686a4a3339b
Niko Juric <nikojuric1998@gmail.com>
2022-02-09 11:59:41 +0100

    fix: stylelint error

--- src/atoms/textfield/styled.js
+++ src/atoms/textfield/styled.js
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 import { pxToRem } from "../../ions/utils/unit";
 
 export const StyledTextField = styled.input`
-	padding: 0rem 1rem;
+	padding: 0 1rem;
 	border: 0;
 	border-radius: ${pxToRem(9)} 0 0 ${pxToRem(9)};
 	background: #fff;
